9005c60108342db788132756bf8644d72bc133df
Zhang Qi <16498871+phresher@users.noreply.github.com>
2017-12-11 16:33:31 +0800

    修改2.3节讲义

--- 2_3/Notes.md
+++ 2_3/Notes.md
@@ -2,9 +2,9 @@
 
 ## 目标
 
-了解透视原理
-
 了解畸变的类型
+
+了解透视原理
 
 了解摄影测量中的误差来源
 
@@ -16,18 +16,36 @@
 
 ## 内容
 
-### 透视原理
-
 ### 畸变
 
-畸变是一种由镜头引起的像差，有三种类型：桶型、枕型和混合型，如下图
+畸变是一种由镜头引起的像差，有三种类型：桶型畸变、枕型畸变和复合畸变，如下图
 
-![桶型畸变](resources/150px-Barrel_distortion.svg.png)
+桶型畸变 ![桶型畸变](resources/150px-Barrel_distortion.svg.png)
 
-![枕型畸变](resources/150px-Pincushion_distortion.svg.png)
+枕型畸变 ![枕型畸变](resources/150px-Pincushion_distortion.svg.png)
 
-![混合型畸变](resources/150px-Mustache_distortion.svg.png)
+复合畸变 ![混合型畸变](resources/150px-Mustache_distortion.svg.png)
+
+### 透视原理
+
+透视原理可以概括为“近大远小”。照片是三维空间的二维投影，单张照片无法测量出一个空间点的三维坐标，要想测量三维坐标必须使用多个机位、多张照片。本课程只考虑摄影测量中最简单的情形——用单张照片测量一个已知平面上的几何参数。
+
+### 摄影测量中的误差来源
+
+影像本身的分辨率（光学分辨率、数码分辨率）、畸变、透视（如俯仰误差）。
+
+### 在 matlab 中矫正畸变
+
+阅读文档《Single Camera Calibration》。
+
+重投影误差的概念，利用重投影误差评估矫正效果，阅读文档《Evaluating the Accuracy of Single Camera Calibration》。
+
+### matlab 图像矩阵的坐标
+
+阅读文档《Image Coordinate Systems》。上面用到的 estimateCameraParameters 可以在矫正畸变的同时建立图片坐标和实际坐标的关系。
 
 ## 家庭作业
 
-阅读维基百科[Distortion (optics)](https://en.wikipedia.org/wiki/Distortion_(optics))
+阅读维基百科 [Distortion (optics)](https://en.wikipedia.org/wiki/Distortion_(optics))
+
+用本课程提供的四幅图校准相机，图中一个格子的尺寸是 100 mm。计算 2.png 中 checkerboard 的尺寸（含白色边框）。
--- 2_3/calibration.m
+++ 2_3/calibration.m
@@ -1,89 +1,16 @@
 % 准备校准图像
 numImages = 4;
-numLiquidColumn = 8;
 files = cell(1, numImages);
 for iImages = 1:numImages
-    files{iImages} = fullfile(sprintf('%d.png', iImages));
+    files{iImages} = fullfile('homework', sprintf('%d.png', iImages));
 end
-% Display one of the calibration images
 
 % 估计摄像机参数
 [imagePoints, boardSize] = detectCheckerboardPoints(files);
-squareSize = 29; % in millimeters 方格图像实际尺寸
+squareSize = 100; % in millimeters 方格图像实际尺寸
 worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 cameraParams = estimateCameraParameters(imagePoints, worldPoints);
-% Evaluate calibration accuracy.
+
+% 评估矫正的准确性
 figure; showReprojectionErrors(cameraParams);
-title('检查投影误差');
-
-% 读取要测量的对象的图像
-imOrig = imread(files{2});
-[im, newOrigin] = undistortImage(imOrig, cameraParams, 'OutputView', 'full');
-
-% 使用图像的 HSV 表示的饱和分量来分割测压管。
-imHSV = rgb2hsv(im);
-saturation = imHSV(:, :, 2);
-t = graythresh(saturation);
-imCoin = (saturation > t);
-figure; imshow(imCoin, 'InitialMagnification', 'fit');
-title('黑白分割图');
-
-% 4检测硬币
-% Find connected components.
-blobAnalysis = vision.BlobAnalysis('AreaOutputPort', true, ...
-    'CentroidOutputPort', false, 'BoundingBoxOutputPort', true, ...
-    'MaximumCount',numLiquidColumn,'MinimumBlobArea', 20, ...
-    'ExcludeBorderBlobs', true);
-
-%{
-AreaOutputPort，CentroidOutputPort，BoundingBoxOutputPort
-默认为true，表示会输出[AREA,CENTROID,BBOX]
-MajorAxisLengthOutputPort，MinorAxisLengthOutputPort，OrientationOutputPort，EccentricityOutputPort，
-EquivalentDiameterSquaredOutputPort，ExtentOutputPort，PerimeterOutputPort，LabelMatrixOutputPort
-
-一些计算结果的输出，默认为false
-OutputDataType，默认double，设置输出数据的格式，包括： double, single, or Fixed point
-Connectivity，默认8，设置哪些像素点是连接的，可选4和8。你懂的，一个小正方形周围有8个小正方形。
-MaximumCount，默认50，解释：Maximum number of labeled regions in each input image（每幅图中最大的区域个数，也就是识别到的运动物体个数）
-MinimumBlobArea，默认0，解释：Minimum blob area in pixels（区域最小面积，占多少个像素？）
-MaximumBlobArea，默认为最大的整型数intmax('uint32')
-Maximum blob area in pixels（区域占的最大面积，单位像素）
-以上两个属性可以控制识别到的区域大小，把不想识别到的筛选出去。
-ExcludeBorderBlobs，默认false
-Exclude blobs that contain at least one border pixel
-Set this property to true if you do not want to label blobs that contain at least one border pixel.
-排除掉有边界的区域。
-    %}
-    
-[areas, boxes] = step(blobAnalysis, imCoin);
-% Sort connected components in descending order by area
-[~, idx] = sort(areas, 'Descend');
-% Get the two largest components.
-boxes = double(boxes(idx(1:numLiquidColumn), :));
-% Adjust for coordinate system shift caused by undistortImage
-boxes(:, 1:2) = bsxfun(@plus, boxes(:, 1:2), newOrigin);
-% 插入测压管标签
-imDetectedCoins = insertObjectAnnotation(im, ...
-    'rectangle', boxes, 'Liquid column');
-figure; imshow(imDetectedCoins, 'InitialMagnification', 'fit');
-title('检测液柱');
-
-% 计算 Extrinsics
-% Detect the checkerboard.
-[imagePoints, boardSize] = detectCheckerboardPoints(im);
-% Compute rotation and translation of the camera.
-[R, t] = extrinsics(imagePoints, worldPoints, cameraParams);
-
-%测量测压管高度
-% Get the top-left and the top-right corners.
-for iLiquidColumn = 1:numLiquidColumn
-    boxj = double(boxes(iLiquidColumn, :));
-    imagePointsLiquidColumn = [boxj(1:2);boxj(1) + boxj(3), boxj(2)];
-    % Get the world coordinates of the corners
-    worldPointsLiquidColumn = pointsToWorld(cameraParams, R, t, imagePointsLiquidColumn);
-    % Compute the diameter of the coin in millimeters.
-    d = worldPointsLiquidColumn(2, :) - worldPointsLiquidColumn(1, :);
-    diameterInMillimeters = hypot(d(1), d(2));
-    fprintf('测量测压管%d的高度= %0.2f mm\n', iLiquidColumn,diameterInMillimeters);
-end
-
+title('检查重投影误差');
